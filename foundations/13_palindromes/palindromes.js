d3da66d81e394d8aa30888424194c724d981e4f9
Olgac Ozsehitoglu <olgacozsehitoglu@gmail.com>
2026-01-02 20:12:00 +0200

    Finish palindrome exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const cleanedStr = str
+    .toLowerCase()
+    .split('')
+    .filter(char => alphanumerical.includes(char))
+    .join('');
+    
+  const checkUpTo = Math.floor(cleanedStr.length / 2);
 
+  for (let i = 0; i < checkUpTo; i++) {
+    if (
+      cleanedStr[i]
+      !== cleanedStr[cleanedStr.length - i - 1]
+    ) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
