26f935c2a8fadc4f2c3b2225a9c8be076d1900bb
Olgac Ozsehitoglu <olgacozsehitoglu@gmail.com>
2026-01-02 18:42:53 +0200

    Finish all tasks

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	return numArray.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  return numArray.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  let product = 1;
+  
+	for (let i = 0; i < exp; i++) {
+    product *= base;
+  }
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
