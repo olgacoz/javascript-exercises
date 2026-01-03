4b07dd9c9c29dbc44bd0c29a9f966aaedf64f818
Olgac Ozsehitoglu <olgacozsehitoglu@gmail.com>
2026-01-03 02:11:09 +0200

    Finish fibonacci task

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(nth) {
+    const num = +nth;
+    
+    if (num < 0 || !Number.isInteger(num)) {
+        return 'OOPS';
+    }
+    switch (num) {
+        case 0: return 0;
+        case 1: return 1;
+    }
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
