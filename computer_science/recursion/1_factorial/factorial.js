64defdd362fe9697a0421ff49d1faf60bd01947b
Olgac Ozsehitoglu <olgacozsehitoglu@gmail.com>
2026-09-20 19:15:23 +0300

    Write recursive factorial function

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,9 @@
-const factorial = function() {
+const factorial = function (n) {
+  if (!Number.isInteger(n) || n < 0) {
+    return undefined;
+  }
 
+  return n == 0 ? 1 : n * factorial(n - 1);
 };
 
 // Do not edit below this line
