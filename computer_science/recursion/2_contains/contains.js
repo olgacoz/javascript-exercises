7b5d4a0fee49ff68cd415d36568a0d27b6c4b8c5
Olgac Ozsehitoglu <olgacozsehitoglu@gmail.com>
2026-09-21 00:03:45 +0300

    Recursive solution ot contains problem

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,16 @@
-const contains = function() {
-  
+const contains = function (obj, target) {
+  const values = Object.values(obj);
+
+  if (values.includes(target)) {
+    return true;
+  }
+
+  nestedObjs = values.filter((value) => {
+    return typeof value === "object" && value !== null;
+  });
+
+  return nestedObjs.some((nestedObj) => contains(nestedObj, target));
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
