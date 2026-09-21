71988090efcf45f653901009085a3022f6b73a99
Olgac Ozsehitoglu <olgacozsehitoglu@gmail.com>
2026-09-21 20:23:17 +0300

    Finish pascal function using recursion

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,17 @@
-const pascal = function() {
-  
+const pascal = function (n) {
+  if (n === 1) {
+    return [1];
+  }
+
+  const prevRow = pascal(n - 1);
+  const prevRowWithZeros = [0, ...prevRow, 0];
+  const currRow = [];
+
+  for (let i = 0; i < n; i++) {
+    currRow[i] = prevRowWithZeros[i] + prevRowWithZeros[i + 1];
+  }
+  return currRow;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
