9b14bed755251bf270e4dfc6f25773d6983f729f
Olgac Ozsehitoglu <olgacozsehitoglu@gmail.com>
2026-09-21 13:51:26 +0300

    Finish totalIntegers problem

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,20 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (obj) {
+  if (typeof obj !== "object" && obj !== null) {
+    return undefined;
+  }
+
+  let intCount = 0;
+  const values = Object.values(obj);
+
+  for (const value of values) {
+    if (Number.isInteger(value)) {
+      intCount++;
+    } else if (typeof value === "object" && value !== null) {
+      intCount += totalIntegers(value);
+    }
+  }
+
+  return intCount;
 };
-  
 // Do not edit below this line
 module.exports = totalIntegers;
